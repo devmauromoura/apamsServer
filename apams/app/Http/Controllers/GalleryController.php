b220921b39301bd579829859995d46f80d04bd64
Mauro Moura <devmauromoura@gmail.com>
2019-06-06 10:24:47 -0400

    Include GooglePhotos

---
+++ apams/app/Http/Controllers/GalleryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ApamsServer\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google\Auth\Credentials\UserRefreshCredentials;
+use Google\Photos\Library\V1\PhotosLibraryClient;
+use Google\Photos\Library\V1\PhotosLibraryResourceFactory;
+
+class GalleryController extends Controller
+{
+    //
+}
