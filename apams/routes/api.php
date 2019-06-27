70ba972afcd1c3f754d0a34e6cf1a21ba3dd306b
Mauro Moura <devmauromoura@gmail.com>
2019-06-27 09:26:07 -0400

    Fix comments routes

--- apams/routes/api.php
+++ apams/routes/api.php
@@ -1,34 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
-
-
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+    ROTAS API APAMS
 */
 
+use Illuminate\Http\Request;
+
+# ROTA PARA REGISTRO DE USUÁRIO
 Route::post('/register', 'Auth\RegisterController@registerApi'); //Cadastrar Usuário
 
+# ROTA PARA LOGIN DE USUÁRIO
 Route::post('/login','Auth\LoginController@loginapi');
 
+# ROTAS COM MIDDLEWARE AUTH, OU SEJA, ACESSIVEIS SOMENTE COM AUTENTICAÇÃO
 Route::group(['middleware' => 'auth:api'], function(){
+    
+    //ROTAS PARA USUÁRIO
     Route::prefix('/user')->group(function(){
         Route::get('/profile','UserController@showProfile');
     });
         Route::post('/update', 'UserController@updateUser');
 
+    //ROTAS PARA ANIMAIS
     Route::prefix('/animals')->group(function(){
         Route::get('/show','AnimalsController@show');
         Route::get('/show/{id}','AnimalsController@showAnimal');
     });
 
+    //ROTAS PARA POSTAGENS
     Route::prefix('/posts')->group(function(){
         Route::get('/show','PostController@showApi');
         Route::get('/show/{id}','PostController@showPost');
--- apams/routes/web.php
+++ apams/routes/web.php
@@ -1,23 +1,35 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+
+/* 
+    ROTAS WEB APAMS
+
+    Obs.: 
+    middleware('check.user') = Validação de o usuário está ativo.
+    middleware('check.google') =  Validação se o usuário possui uma conta Google.
+
 */
 
+
+# ROTA PARA TELA DE LOGIN
 Route::get('/','Auth\LoginController@index')->name('index');
+
+Route::get('callback', 'Auth\LoginController@callback');
+Route::prefix('/auth')->group(function () {
+    Route::get('google', 'Auth\LoginController@redirect');
+    Route::name('logout')->post('logout', 'LoginController@logout');
+});
+
+#ROTA PARA VALIDAR LOGIN E SAIR
 Route::post('/login','Auth\LoginController@login');
 Route::get('/sair', function(){ Auth::logout(); return redirect(''); });
 
+
+# ROTAS COM MIDDLEWARE AUTH, OU SEJA, ACESSIVEIS SOMENTE COM AUTENTICAÇÃO
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/home','homeController@show');
     Route::get('/configuracoes','ConfiguracoesController@show')->middleware('check.user');
 
+    // ROTAS DE USUÁRIO
     Route::prefix('/users')->group(function(){
         Route::get('/', 'UserController@show');
         Route::match(['get', 'post'], '/register', 'Auth\RegisterController@register');
@@ -25,6 +37,7 @@ Route::group(['middleware' => 'auth'], function(){
         Route::get('/delete', 'UserController@delete');
     });
 
+    // ROTAS DE ANIMAIS
     Route::prefix('/animais')->group(function(){
         Route::get('/','AnimalsController@showWeb');
         Route::match(['get', 'post'], '/cadastro', 'AnimalsController@registerWeb')->middleware('check.google');
@@ -32,11 +45,13 @@ Route::group(['middleware' => 'auth'], function(){
         Route::post('/delete', 'AnimalsController@registerWeb');
     });
 
+    // ROTAS DE PATROCINADORES
     Route::prefix('/patrocinadores')->group(function(){
         Route::post('/cadastrar', 'SponsorsController@register')->middleware('check.google');
         Route::post('/atualizar', 'SponsorsController@update')->middleware('check.google');
     });
 
+    // ROTAS DE POSTAGENS
     Route::prefix('/postagens')->group(function(){
         Route::get('/','PostController@show')->middleware('check.user');
         Route::match(['get', 'post'], '/create', 'PostController@create');
@@ -44,6 +59,7 @@ Route::group(['middleware' => 'auth'], function(){
         Route::post('/delete','PostController@delete');
     });
 
+    // ROTAS DE NOTIFICAÇÕES
     Route::prefix('/notificacoes')->group(function(){
         Route::get('/','NotificationController@show')->middleware('check.user');
         Route::post('/create','NotificationController@create');
@@ -52,27 +68,11 @@ Route::group(['middleware' => 'auth'], function(){
 
 
 
-
-
-
-
-
-Route::get('callback', 'Auth\LoginController@callback');
-Route::prefix('/auth')->group(function () {
-    Route::get('google', 'Auth\LoginController@redirect');
-    Route::name('logout')->post('logout', 'LoginController@logout');
-});
-
-
-
-
+// IGNORAR
 Route::middleware('auth')->group(function () {
     Route::resource('albums', 'AlbumController');
-
     Route::resource('mediaitems', 'MediaController');
-
     Route::name('mediaitems.album')->get('mediaitems/album/{id}', 'MediaController@album');
-
     Route::view('upload', 'form')->name('form');
     Route::name('upload')->post('upload', 'UploadController');
 });
