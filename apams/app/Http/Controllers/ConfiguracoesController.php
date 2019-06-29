15e3f3ea67a64e8f1e0dc1d28b5fa06dda706cf0
Mauro Moura <devmauromoura@gmail.com>
2019-06-29 03:38:33 -0300

    Insert update access token user

--- apams/app/Http/Controllers/ConfiguracoesController.php
+++ apams/app/Http/Controllers/ConfiguracoesController.php
@@ -10,7 +10,34 @@ use Auth;
 use GuzzleHttp\Client;
 
 class ConfiguracoesController extends Controller
+
 {
+
+    public function access(){
+        
+        $tokenOr = User::where('email','devmauromoura@gmail.com')->get();
+        $result =  json_decode($tokenOr);
+        $tokenfn = $result[0]->refresh_token;
+
+        $access = new Client();
+        $responseAccess = $access->post('https://www.googleapis.com/oauth2/v4/token', [
+            'form_params' => [
+                'refresh_token' => $tokenfn,
+                'client_id' => '1006479685201-bji1q1egg2dcqonnu0c14jkbghuokbb5.apps.googleusercontent.com',
+                'client_secret' => 'TJPAWTUbMobioLBwsqCIrnAR',
+                'grant_type' => 'refresh_token',
+            ],
+        ]);
+
+        $responseAccess = json_decode($responseAccess->getBody(), true);
+
+        $up = User::find(2);
+        $up->access_token = $responseAccess['access_token'];
+        $up->save();
+
+    }
+
+
     public function convert(){
 
         $ac = Animals::count();
@@ -34,6 +61,7 @@ class ConfiguracoesController extends Controller
     }
 
     public function show(){
+         $this->access();
 
         //$this->convert();
 
