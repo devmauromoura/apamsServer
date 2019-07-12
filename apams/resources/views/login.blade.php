e8eb273917136a4e5928a19e65729140405c3814
Mauro Moura <devmauromoura@gmail.com>
2019-07-11 22:23:16 -0300

    Test Route

---
+++ apams/resources/views/login.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ADM APAMS</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <main>
+
+        <section banner>
+        </section>
+
+        <section login>
+
+            <div logo>
+                <img src="./img/logo.png" alt="APAMS" height="150px">
+            </div>
+
+            <div class="title">
+                <h3>Central de gerenciamento APP</h3>
+            </div>
+            @if(session()->has('error'))
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                {{ session()->get('error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endif
+            <form action="/login" method="post">
+                @csrf
+                <div class="form-group">
+                    <label>Endereço de email</label>
+                    <input type="email" class="form-control" name="email" placeholder="Seu email">
+                </div>
+                <div class="form-group">
+                    <label>Senha</label>
+                    <input type="password" class="form-control" name="password" placeholder="Senha">
+                </div>
+                <button type="submit" class="btn btn-block">Entrar</button>
+                <a href="/auth/google" class="btn btn-block">Entrar com conta Google</a>
+            </form>
+
+        </section>
+
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
