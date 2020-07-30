1b6193e679b6311a17ae406fe6f3251147a6b331
Mauro Moura <devmauromoura@gmail.com>
2020-07-30 01:40:17 -0300

    fix: ajuste dos seeders

--- apams/database/seeds/DatabaseSeeder.php
+++ apams/database/seeds/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(UsersSeeder::class);
+        //$this->call(UsersSeeder::class);
         $this->call(StaffSeeder::class);
-        $this->call(PostSeeder::class);
-        $this->call(AnimalSeeder::class);
-        $this->call(SettingsSeeder::class);
-        $this->call(SponsorSeeder::class);
+        // $this->call(PostSeeder::class);
+        // $this->call(AnimalSeeder::class);
+        // $this->call(SettingsSeeder::class);
+        // $this->call(SponsorSeeder::class);
     }
 }
